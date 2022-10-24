c6f7cd910b89526dc4e34e4f6f22d465b7834a83
Riley1101 <www.riley.s.eileen@gmail.com>
2022-10-24 18:32:03 +0630

    🧪 Testing incremental build

---
+++ src/components/home/Foods.svelte
@@ -0,0 +1,14 @@
+<script>
+    import Card from '@components/common/Card.svelte'
+    export let data;
+    export let title;
+    export const revalidate = 100; 
+</script>
+<main >
+    <h1 class="text-3xl m-4 mt-12 mb-4 font-bold border-b-4 border-blue-500 max-w-max">{title}</h1>
+  <div class='grid grid-cols-1 md:grid-cols-4 gap-4 p-4'>
+    {#each data as food}
+        <Card food={food} />
+    {/each}
+  </div>
+</main>
