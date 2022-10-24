e26abbecd38db018c440581f040b79a1d8c12101
Riley1101 <www.riley.s.eileen@gmail.com>
2022-10-24 14:44:39 +0630

    👑Initial commit on repo with svelte + astro

---
+++ src/components/Card.svelte
@@ -0,0 +1,23 @@
+<script lang='ts'>
+	import type { FoodTypes } from '@components/types';
+    export let food:FoodTypes;
+</script>
+<div class='p-4 flex flex-col border rounded-md'>
+    <div class='w-full aspect-video bg-gray-200'>
+    </div>
+    <div class=' flex justify-between my-4'>
+    <span class='text-md  rounded-full'>{food.price}$</span>
+    {#if food.tags}
+        <div class="flex items-center gap-1">
+            {#each food.tags as tag}
+                <span class='text-sm bg-teal-200 px-2 py-1 rounded-full'>
+                    {tag}
+                </span>
+            {/each}
+        </div>
+    {/if}
+    </div>
+    <p class="text-xl mb-2">{food.title}</p>
+    <p class="text-md text-gray-500">{food.summary.en}</p>
+    <button on:click={()=>alert('yes')} class='mt-4 hover:border-l-4 transition-all  border-lime-500 max-w-max px-8 ml-auto py-1 rounded-full  bg-blue-500 text-white'>BUY</button>
+</div>
---
+++ src/lib/sanityClient.ts
@@ -0,0 +1,9 @@
+import sanityClient from "@sanity/client";
+
+const client = sanityClient({
+  projectId: import.meta.env.SANITY_PROJECT_ID,
+  dataset: import.meta.env.SANITY_DATASET,
+  apiVersion: import.meta.env.SANITY_API_VERSION,
+  useCdn: true,
+});
+export default client;
